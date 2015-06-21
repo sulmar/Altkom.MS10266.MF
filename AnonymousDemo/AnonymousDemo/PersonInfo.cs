6c675fe8453a8d6986d4c1d86403351ab789bf94
Marcin Sulecki <marcin.sulecki@gmail.com>
2015-06-21 22:18:18 +0200

    Init

---
+++ AnonymousDemo/AnonymousDemo/PersonInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnonymousDemo
+{
+    class PersonInfo
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NIP { get; set; }
+    }
+}
---
+++ AnonymousDemo/AnonymousDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace AnonymousDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var birthDay = DateTime.Parse("2008-06-12");
+
+            var x = 10;
+            
+            var person1 = new
+            {
+                FirstName = "Marcin",
+                LastName = "Sulecki",
+                NIP = "9531204591",
+                BirthDay = DateTime.Parse("2008-06-12"),
+                HairColor = "Red",
+                Salary = 123000.00m,
+            };
+
+            var person2 = new
+            {
+                FirstName = "Bartek",
+                LastName = "Sulecki",
+                NIP = "95221204591",
+                BirthDay = DateTime.Parse("2008-06-12"),
+                HairColor = "Brown",
+                Salary = 999.00m,
+            };
+
+            var person3 = new
+            {
+                FirstName = "Piotr",
+                LastName = "Wozniak",
+                NIP = "11221204591",
+                BirthDay = DateTime.Parse("2001-06-12"),
+                HairColor = "Red",
+                Salary = 110000.00m,
+            };
+
+            ArrayList persons = new ArrayList();
+            persons.Add(person1);
+            persons.Add(person2);
+            persons.Add(person3);
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+
+
+
+
+        }
+    }
+}
