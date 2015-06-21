b6f7b84ae521b0a36c8124f21a573ea96356b9e7
Marcin Sulecki <marcin.sulecki@gmail.com>
2015-06-21 22:22:04 +0200

    Init

---
+++ ExtensionMethodDemo/ExtensionMethodDemo/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethodDemo
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime AddWorkingDays(this DateTime date, int days)
+        {
+            return date.AddDays(days);
+        }
+
+        public static bool IsHoliday(this DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Saturday ||
+                date.DayOfWeek == DayOfWeek.Sunday;
+        }
+
+        public static int GetSize(this string text)
+        {
+            return text.Length;
+        }
+    }
+}
---
+++ ExtensionMethodDemo/ExtensionMethodDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime endDate = DateTime.Today.AddDays(0);
+            endDate = endDate.AddWorkingDays(10);
+
+            // Fluent Datetime
+            endDate = endDate.Hour(2).Minut
+            
+            if (endDate.IsHoliday())
+            {
+                Console.WriteLine("Wolne :)");
+            }
+            else 
+            {
+                Console.WriteLine("znow do pracy... :-/");
+            }
+            
+        }
+
+    }
+}
