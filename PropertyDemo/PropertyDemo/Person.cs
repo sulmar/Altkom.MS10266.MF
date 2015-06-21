c58d200c1ca20fc1819f93fce9dc252ea47c4ac5
Marcin Sulecki <marcin.sulecki@gmail.com>
2015-06-21 22:23:23 +0200

    Init

---
+++ PropertyDemo/PropertyDemo/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PropertyDemo
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public decimal Salary { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+
+
+        private string lastName;
+
+        // Property
+
+        public string LastName
+        {
+            set
+            {              
+                this.lastName = value;
+            }
+
+            get
+            {
+                return this.lastName;
+            }
+        }
+
+        private string password;
+
+        public void SetPassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentNullException("Password");
+            }
+
+            this.password = password;
+        }
+
+        public string GetPassword()
+        {
+            return this.password;
+        }
+
+
+        
+
+        private byte age;
+
+
+
+    }
+}
---
+++ PropertyDemo/PropertyDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PropertyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.SetPassword("1234");
+
+            person.LastName = "Sulecki";
+
+            Console.WriteLine(person.GetPassword());
+
+            Console.WriteLine(person.LastName);
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
