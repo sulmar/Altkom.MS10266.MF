f19e4ce8eb3f8c965437f50ea52045b7f96d3949
Marcin Sulecki <marcin.sulecki@gmail.com>
2015-06-21 22:23:44 +0200

    Init

---
+++ UsingDemo/UsingDemo/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace UsingDemo
+{
+    class DataService : IDisposable
+    {
+        private string certFileName;
+
+        private XmlDocument doc;
+
+        public void Send()
+        {
+            doc = new XmlDocument();
+
+            doc.Load(certFileName);
+
+            Console.WriteLine(String.Format("sending {0} ...", certFileName));
+        }
+
+        public void Prepare(string certFileName)
+        {
+            this.certFileName = certFileName;
+
+           
+           
+
+        }
+
+        public void Dispose()
+        {
+            File.Delete(certFileName);
+        }
+    }
+}
---
+++ UsingDemo/UsingDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generate();
+        }
+
+        private static void Generate()
+        {
+            try
+            {
+                using (DataService service = new DataService())
+                {
+                    string filename = @"C:\Users\Student\Documents\testXXX.xml";
+
+                    service.Prepare(filename);
+                    service.Send();
+
+                    Console.WriteLine("success.");
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        
+
+        }
+    }
+}
